f17764c3e5c6361e102ae985d4f19839ece0ab77
Alejandro Bello <62668402+AlejandroBelloIglesias@users.noreply.github.com>
2023-07-20 10:54:46 +0200

    Add files via upload

---
+++ UIView+Extensions.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+/**
+ More readable constraints using builder pattern
+*/
+extension UIView {
+
+    @discardableResult
+    func top(to anchor: NSLayoutYAxisAnchor, padding: CGFloat = 0) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: anchor, constant: padding).isActive = true
+        return self
+    }
+
+    @discardableResult
+    func bottom(to anchor: NSLayoutYAxisAnchor, padding: CGFloat = 0) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        bottomAnchor.constraint(equalTo: anchor, constant: padding).isActive = true
+        return self
+    }
+
+    @discardableResult
+    func leading(to anchor: NSLayoutXAxisAnchor, padding: CGFloat = 0) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        leadingAnchor.constraint(equalTo: anchor, constant: padding).isActive = true
+        return self
+    }
+
+    @discardableResult
+    func trailing(to anchor: NSLayoutXAxisAnchor, padding: CGFloat = 0) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        trailingAnchor.constraint(equalTo: anchor, constant: padding).isActive = true
+        return self
+    }
+
+    @discardableResult
+    func height(_ height: CGFloat) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+        return self
+    }
+
+    @discardableResult
+    func width(_ width: CGFloat) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+        return self
+    }
+}
+
+
+class Example {
+
+    let background = UIView();
+    let footer = UIView();
+
+    background
+        .leading(to: view.leadingAnchor)
+        .trailing(to: view.trailingAnchor, padding: 13)
+        .bottom(to: view.bottomAnchor)
+        .height(FooterView.heightWithMargin)
+
+    footer
+        .leading(to: background.leadingAnchor)
+        .trailing(to: background.trailingAnchor)
+        .top(to: background.topAnchor)
+        .height(100))
+}
