be10cdb92eaa5a2d8ee276b9558ed96dd65cb620
Alejandro Bello <62668402+AlejandroBelloIglesias@users.noreply.github.com>
2023-10-13 09:42:29 +0200

    Create UIColor+AppColors.swift

---
+++ UIColor+AppColors.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension UIColor {
+    static let strawberry = UIColor(named: "strawberry")!
+    static let adjWhite = UIColor(named: "AdjustableWhite")!
+}
+
+
+//Usage:
+UIColor.strawberry
+//instead of
+UIColor(named: "strawberry")!
+
